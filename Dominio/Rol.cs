7e6db15f5c9eac54c57814664981569313914365
jvelazquez <jonathanvgms@gmail.com>
2024-10-08 11:07:43 -0300

    Creacion proyecto WebApi

---
+++ Dominio/Rol.cs
@@ -0,0 +1,8 @@
+﻿namespace Dominio;
+
+public class Rol
+{
+    public int IdRol { get; set; }
+    public required string Nombre { get; set; }
+    public List<Usuario> Usuarios { get; set; } = new List<Usuario>();
+}
---
+++ Dominio/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace Dominio;
+
+public class Usuario
+{
+    public int IdUsuario { get; set; }
+    public required string Nombre { get; set; }
+    public required string NombreUsuario { get; set; }
+    public required string Contraseña { get; set; }
+    public required string Email { get; set; }
+    List<Rol> Roles { get; set; } = new List<Rol>();
+}
